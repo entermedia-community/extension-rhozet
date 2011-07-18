a03008ef9954c0e57f29bb81ea39983f70d656f3
cburkey <cburkey@entermediasoftware.com>
2011-07-18 10:15:38 -0400

    Michael tweaks

--- etc/install.js
+++ etc/install.js
@@ -3,7 +3,7 @@ importPackage( Packages.java.util );
 importPackage( Packages.java.lang );
 importPackage( Packages.com.openedit.modules.update );
 
-var war = "http://dev.entermediasoftware.com/projects/entermedia-rhozet/entermedia-rhozet.zip";
+var war = "http://dev.entermediasoftware.com/jenkins/job/extension-rhozet/lastSuccessfulBuild/artifact/deploy/extension-rhozet.zip";
 
 var root = moduleManager.getBean("root").getAbsolutePath();
 var web = root + "/WEB-INF";
